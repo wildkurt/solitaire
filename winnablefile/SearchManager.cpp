8c21f6f15a2bd811c05bd8e9df78bc6d08d4faf4
Wendell Best <35972430+wildkurt@users.noreply.github.com>
2023-06-12 23:01:58 -0500

    Update SearchManager.cpp
    Updating the files manually to make sure the CLion files do not upload to Git

--- winnablefile/SearchManager.cpp
+++ winnablefile/SearchManager.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "SearchManager.h"
+SearchManager::SearchManager(GameConfiguration *gam, SearchSettings set) {
+    game = gam;
+    settings = set;
+}
+
+bool SearchManager::run() {
+    GameConfiguration recursiveGame;
+    recursiveGame = *game;
+    if(recursiveSearch(recursiveGame))
+        return true;
+    else
+        return false;
+}
+
+bool SearchManager::recursiveSearch(GameConfiguration recGame) {
+    //check for winning condition
+    if(recGame.gameInWinningConfiguration()){
+        *game = recGame;
+        return true;
+    }
+    //Check for safe moves for every tested move
+    return false;
+}
