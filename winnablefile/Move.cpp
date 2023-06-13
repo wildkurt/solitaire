58f510a8c350b1f6fdf75bb62d4887860882feed
Wendell Best <35972430+wildkurt@users.noreply.github.com>
2023-06-12 23:07:48 -0500

    Update Move.cpp
    update 6

--- winnablefile/Move.cpp
+++ winnablefile/Move.cpp
@@ -8,11 +8,4 @@ Move::Move() {from = '0'; to = '0';}
 
 Move::Move(char f, char t) {from = f; to = t;}
 
-void Move::printMove(std::ofstream *file) {
-    if(from == '.')
-        *file << '.';
-    else if(from == 'r')
-        *file << 'r';
-    else
-        *file << from << "->" << to;
-}
+
