93fd6158f154097e1f89768d1421942fc2b5ec49
Wendell Best <35972430+wildkurt@users.noreply.github.com>
2023-06-12 23:03:15 -0500

    Update SearchManager.h
    Manual update 2

--- winnablefile/SearchManager.h
+++ winnablefile/SearchManager.h
@@ -4,11 +4,24 @@
 
 #ifndef SOLITAIRE_SEARCHMANAGER_H
 #define SOLITAIRE_SEARCHMANAGER_H
-
+#include <map>
+#include "gameConfiguration.h"
+#include "SearchSettings.h"
 
 class SearchManager {
-
+private:
+    GameConfiguration *game;
+    SearchSettings settings;
+    int configurations = 0;
+    std::map <int, GameConfiguration> hashtable;
+    std::string cardFrom = "w1234567.r";
+    std::string cardTo = "1234567f";
+public:
+    SearchManager(GameConfiguration *gam, SearchSettings set);
+    bool run();
+    bool recursiveSearch(GameConfiguration game);
 };
+
 
 
 #endif //SOLITAIRE_SEARCHMANAGER_H
