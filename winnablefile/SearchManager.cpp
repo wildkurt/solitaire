143bd801cf61166fce21ad7d402617f26601e45b
wildkurt <wendellkbest@gmail.com>
2023-06-22 16:59:27 -0500

    Figuring out the logic of the recursive search

--- winnablefile/SearchManager.cpp
+++ winnablefile/SearchManager.cpp
@@ -24,5 +24,24 @@ bool SearchManager::recursiveSearch(GameConfiguration recGame) {
         return true;
     }
     //Check for safe moves for every tested move
+    for(int i = 0; i < cardFrom.size(); i++){
+        for(int j = 0; j < cardTo.size(); j++){
+            //Don't check if from and to are sae place
+            if(cardFrom[i] == cardTo[j]){
+                continue;
+            }
+            else{
+                /* 1. Create a move
+                 * 2. If cache mode is selected, create hash of moves including new one
+                 *      a. Check hash table
+                 *      b. If it exists, continue
+                 *      c. If it doesn't exist, test new move
+                * 3. Things to update:
+                 *  a. Number of moves, and determine if more moves remain
+                 *  b. Number of configurations
+                 *  c. remaining waste resets*/
+            }
+        }
+    }
     return false;
 }
